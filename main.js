bd162c3beff78c01590d910a131133fd1c12568c
Daniel <danielsumah@gmail.com>
2021-03-31 01:38:01 +0100

    create benefit page for both pc and mobile

--- main.js
+++ main.js
@@ -1,3 +1,5 @@
+
+// Mobile navbar toggle code
 function toggleNavbar(){
   var x = document.getElementById("mobile-menu");
   if (x.className === "mobile-menu") {
@@ -5,4 +7,15 @@ function toggleNavbar(){
   } else {
     x.className = "mobile-menu";
   }
+}
+
+
+// Change Content of Benefit on solar panel page 
+// when width is less or equal to 600px
+if (screen.width <= 600){
+  document.querySelector('.benefit-l1').innerHTML = '1. Our Services are affordable and reliable.';
+  document.querySelector('.benefit-l2').innerHTML = '2. We take your safety into consideration while installling your renewable energy system.';
+  document.querySelector('.benefit-l3').innerHTML = '3. We provide integrity services on your systems even after installing them.';
+  document.querySelector('.benefit-l4').innerHTML = '4. Quick and responsive customer support is available to all our Customers.';
+
 }
